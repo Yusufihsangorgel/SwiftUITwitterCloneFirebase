5d6b15672441f272a00bbadee6723987282a8e54
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-09-15 15:52:21 +0300

    tweet bug fixed

--- TwitterCloneFirebase/Model/Tweet.swift
+++ TwitterCloneFirebase/Model/Tweet.swift
@@ -11,7 +11,7 @@ import Firebase
 struct Tweet: Identifiable,Decodable{
     @DocumentID var id: String?
     let caption: String
-    let timestamp: Timestamp
+    let timestap: Timestamp
     let uid: String
     var likes: Int
     var user: User?
